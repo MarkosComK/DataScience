e390f77b055b8734b3ffe3c5688b3ea917809c0c
MarkosComK <markoscomks@gmail.com>
2024-12-09 11:08:36 +0000

    stablished connection with db

--- ex02/table.py
+++ ex02/table.py
@@ -1,4 +1,19 @@
 import pandas
-import psycopg2
+import psycopg2 as psy
 
-print("Hello pandas")
+
+def connect_db():
+	connection = psy.connect(
+		dbname="piscineds",
+		user="marsoare",
+		password="secret",
+		host="localhost",
+		port="5432"
+	)
+	print(f"Connection successful {connection}")
+	connection.close()
+
+def __main__():
+	connect_db()
+
+__main__()
