83032a82ae43481b9320671b444192e01946bf25
MarkosComK <markoscomks@gmail.com>
2024-12-09 11:14:34 +0000

    good practices

--- ex02/table.py
+++ ex02/table.py
@@ -2,18 +2,19 @@ import pandas
 import psycopg2 as psy
 
 
-def connect_db():
-	connection = psy.connect(
-		dbname="piscineds",
-		user="marsoare",
-		password="secret",
-		host="localhost",
-		port="5432"
-	)
+def connect_db(db_params):
+	connection = psy.connect(**db_params)
 	print(f"Connection successful {connection}")
 	connection.close()
 
 def __main__():
-	connect_db()
+	db_params = {
+		"dbname":"piscineds",
+		"user":"marsoare",
+		"password":"secret",
+		"host":"localhost",
+		"port":"5432"
+	}
+	connect_db(db_params)
 
 __main__()
