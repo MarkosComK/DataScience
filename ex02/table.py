eca03639da8920c7309d1b6e921d1e19aa5de56d
MarkosComK <markoscomks@gmail.com>
2024-12-09 11:27:57 +0000

    Read csv first line properly

--- ex02/table.py
+++ ex02/table.py
@@ -3,9 +3,18 @@ import psycopg2 as psy
 
 
 def connect_db(db_params):
-	connection = psy.connect(**db_params)
-	print(f"Connection successful {connection}")
-	connection.close()
+	try:
+		with psy.connect(**db_params) as connection:
+			print(f"Connection successful {connection}")
+			if connection.closed != 0:
+				connection.close()
+	except psy.connect as error:
+		print(f"Connection failed {error}")
+
+def open_file(file_path):
+	with open(file_path, "r") as file:
+		line = file.readline()
+		print(line)
 
 def __main__():
 	db_params = {
@@ -15,6 +24,8 @@ def __main__():
 		"host":"localhost",
 		"port":"5432"
 	}
+	open_file("../subject/customer/data_2022_dec.csv")
 	connect_db(db_params)
 
 __main__()
+
